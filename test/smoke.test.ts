50782a06ee91ec8fbd94845cfcb4d2ab2666a158
Indra <indra@coderbuzz.com>
2026-06-21 10:31:30 +0000

    chore: add CI workflow + smoke test

---
+++ test/smoke.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "bun:test";
+import { encode, decode, encodedSize } from "@coderbuzz/msgpack";
+
+test("encode/decode string", () => {
+  const buf = encode("hello");
+  expect(decode(buf)).toBe("hello");
+});
+
+test("encode/decode object", () => {
+  const val = { a: 1, b: "x", c: [1, 2] };
+  expect(decode(encode(val))).toEqual(val);
+});
+
+test("encodedSize matches", () => {
+  const val = { name: "test", count: 42 };
+  expect(encodedSize(val)).toBe(encode(val).length);
+});
